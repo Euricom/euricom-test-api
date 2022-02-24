5739762fe0d9c491117aeb5cdca527f80d4fba20
Jonas <boone.jonas@outlook.be>
2022-02-24 09:32:19 +0100

    fix: invalid remove item from basket behaviour

--- src/graphql/basket/basket.resolver.js
+++ src/graphql/basket/basket.resolver.js
@@ -1,4 +1,4 @@
-const { getOrCreateBasket, clearBasket } = require('../../data/basket');
+const { getOrCreateBasket, clearBasket, removeProductFromBaskets } = require('../../data/basket');
 const { UserInputError } = require('apollo-server-express');
 const { BusinessRuleError } = require('../errors');
 
@@ -62,12 +62,19 @@ const basketResolvers = {
     removeItemFromBasket: (root, args) => {
       const productId = Number(args.input.productId);
       let basket = getOrCreateBasket(args.input.checkoutID);
-      const index = basket.find((item) => item.id === productId);
+      // item.productId !== index.productId instead of item.id !== index.id to correctly target products
+      const index = basket.find((item) => item.productId === productId);
       if (!index) {
         throw new UserInputError('Product not found');
       }
+
+      // this function was declared in basket.js but never used resulting in two different basket arrays between query and mutation
+      // when a user tries to delete a product that has been deleted earlier, an error is shown (product not found)
+      removeProductFromBaskets(args.input.productId);
+
       // console.log(index, basket.filter((item) => item.id !== index.id));
-      basket = basket.filter((item) => item.id !== index.id);
+      // item.productId !== index.productId instead of item.id !== index.id to correctly target products
+      basket = basket.filter((item) => item.productId !== index.productId);
       const newBasket = {
         checkoutID: args.input.checkoutID,
         items: basket,
